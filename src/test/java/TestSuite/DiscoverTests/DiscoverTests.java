156979a8d85aa65a8f17e0a2d4f8971d780ddd24
swapnilGirishPawar <swapnil.girish@anywhere.co>
2026-04-15 01:07:02 +0530

    removed dead code from DiscoverTests

--- src/test/java/TestSuite/DiscoverTests/DiscoverTests.java
+++ src/test/java/TestSuite/DiscoverTests/DiscoverTests.java
@@ -15,24 +15,4 @@ public class DiscoverTests extends Hooks {
         driver = Hooks.getDriver();
         discover = new DiscoverPage(driver);
     }
-
-    @Test
-    public void shouldShowEmptyGamesStateOnDiscover() {
-        discover.tapDiscoverTab();
-        discover.assertDiscoverTabSelected();
-        discover.assertNoAppsEmptyStateVisible();
-        discover.assertVpnDisabledHintVisible();
-    }
-    @Test
-    public void shouldOpenSurveyFlowFromCard() {
-        discover.tapCompleteSurveysCard();
-        // add assertions on the next screen in that screen’s page object
-    }
-    @Test
-    public void shouldOpenEarnSections() {
-        discover.tapEarnWithApps();
-        // assert next screen
-        discover.tapEarnWithGames();
-        // assert next screen
-    }
 }
